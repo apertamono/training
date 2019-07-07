5b5a1e3d31ddcc8b9e626122b56c420b26ce6f4f
Edwin den Boer <woordenboer@planet.nl>
2019-07-07 16:37:39 +0200

    My first python program
    Created during the Learn to Code Meetup in restaurant Speys, Jaarbeurs, Utrecht, NL.

---
+++ Python/filelist-shuffled.py
@@ -0,0 +1,28 @@
+import os
+import random
+
+# My first python program
+# Create a list of image files in multiple directories and subdirectories
+# Shuffle the list randomly before you print it to a text file
+# Paths are defined for Windows
+# Tested in Python 3.5.2 and 3.6.4
+# Could the condition in line 17 be more compact?
+
+path = 'C:\\Data\\Images\\Memes\\'
+
+files = []
+def listfunction():
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.jpg' in file or '.png' in file or '.gif' in file or '.jpeg' in file:
+                files.append(os.path.join(r, file))
+
+listfunction()
+
+path = 'C:\\Data\\Images\\Cringe\\'
+listfunction()
+        
+random.shuffle(files)
+for f in files:
+    print(f)
