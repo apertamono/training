925b5e287df8ec6c838e1822fc818097013bdbe7
Edwin den Boer <woordenboer@planet.nl>
2019-07-08 14:25:42 +0200

    More comments

--- Python/filelist-shuffled.py
+++ Python/filelist-shuffled.py
@@ -2,11 +2,18 @@ import os
 import random
 
 # My first python program
-# Create a list of image files in multiple directories and subdirectories
+# Create a list of image files from multiple directories and subdirectories
 # Shuffle the list randomly before you print it to a text file
+
+# Limitations: 
 # Paths are defined for Windows
 # Tested in Python 3.5.2 and 3.6.4
-# Could the condition in line 18 be more compact?
+# Instead of matching a substring, I should use pathlib or splitext
+# Could the if-statement be more compact?
+# Could the path be passed to a function?
+# There were errors that stopped execution at filenames with Russian or Polish letters
+# This code skips extensions in upper case, but I should run another script to change them to lower case anyway
+# In production, print("  ", f) adds two spaces before every filename in order to log which images I used
 
 path = 'C:\\Data\\Images\\Memes\\'
 
